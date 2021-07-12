201eba7cb2e142fb44fe3a4f8334555998740c81
Mauro Rezende <maurorezende2012@gmail.com>
2021-07-12 11:54:58 -0300

    entity resource

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Offer.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Offer.java
@@ -2,6 +2,8 @@ package com.devsuperior.dslearnbds.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,25 +11,28 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_offer")
 public class Offer implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String edition;
 	private Instant startMoment;
 	private Instant endMoment;
-	
-	
+
 	@ManyToOne
 	@JoinColumn(name = "course_id")
 	private Course course;
-	
+
+	@OneToMany(mappedBy = "offer")
+	private List<Resource> resources = new ArrayList<>();
+
 	public Offer() {
 	}
 
@@ -80,6 +85,10 @@ public class Offer implements Serializable {
 		this.course = course;
 	}
 
+	public List<Resource> getResources() {
+		return resources;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -105,5 +114,4 @@ public class Offer implements Serializable {
 		return true;
 	}
 
-	
 }
